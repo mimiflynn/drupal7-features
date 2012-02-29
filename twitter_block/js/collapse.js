cd3a0614a8f129ab8088a7937efb858d774ace91
Mimi Flynn <mimi.tokyo@black-monolith.com>
2012-02-28 20:45:36 -0500

    Twitter cookie js

--- twitter_block/js/collapse.js
+++ twitter_block/js/collapse.js
@@ -1,7 +1,20 @@
 (function ($) {
   $(document).ready(function() {
-    $('.block-twitter-pull .tweets-pulled-listing h2').append('<span>off X</span>').click(function(){
-      $(this).next().toggle();
+    if ($.cookie('twitter') === 'closed') {
+      $('.block-twitter-pull .tweets-pulled-listing h2').append('<span>+on</span>').addClass('closed').next().hide();
+    } else {
+      $('.block-twitter-pull .tweets-pulled-listing h2').append('<span>offX</span>');
+    }
+    $('.block-twitter-pull .tweets-pulled-listing h2').click(function(){
+      if ($(this).hasClass('closed')) {
+        $(this).next().show();
+        $.cookie('twitter', 'open', { path: '/' });
+        $(this).removeClass('closed').addClass('open').children('span').text('offX');
+      } else {
+        $(this).next().hide();
+        $.cookie('twitter', 'closed', { path: '/' });
+        $(this).removeClass('open').addClass('closed').children('span').text('+on');
+      }
     });
   });
 })(jQuery);
